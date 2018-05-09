22158b3200701e077b2e8dbfe259a0f7669bdd77
Philipp Ploder <philipp.ploder@outlook.com>
2018-05-09 17:39:15 +0200

    Remove all non-target end states

--- vendingmachine.py
+++ vendingmachine.py
@@ -2,6 +2,8 @@ from fire import Fire
 
 from vm_gen import create_graph
 
+DEFAULT_OUT = 'vending_machine.dot'
+
 
 def report(total, increments, initial, n, e):
     print(f'''
@@ -21,18 +23,18 @@ Edges: {e}
 
 class VendingMachineGenerator:
     @staticmethod
-    def save(total, increments, initial=0, out='vendingmachine.dot'):
-        g, n, e = create_graph(total, increments, initial)
+    def save(target, increments, initial=0, out=DEFAULT_OUT):
+        g, n, e = create_graph(target, increments, initial)
         g.save(filename=out)
 
-        report(total, increments, initial, n, e)
+        report(target, increments, initial, n, e)
 
     @staticmethod
-    def render(total, increments, initial=0, out='vendingmachine.dot', view=False):
-        g, n, e = create_graph(total, increments, initial)
+    def render(target, increments, initial=0, out=DEFAULT_OUT, view=False):
+        g, n, e = create_graph(target, increments, initial)
         g.render(filename=out, view=view)
 
-        report(total, increments, initial, n, e)
+        report(target, increments, initial, n, e)
 
 
 if __name__ == '__main__':
--- vm_gen.py
+++ vm_gen.py
@@ -1,8 +1,10 @@
+from functools import reduce
+
 from graphviz import Digraph
 
 
-def create_graph(total, steps, initial=0):
-    nodes, edges = create_graph_components(initial, steps, total)
+def create_graph(target, steps, initial=0):
+    nodes, edges = create_graph_components(initial, steps, target)
 
     g = Digraph()
 
@@ -15,7 +17,7 @@ def create_graph(total, steps, initial=0):
     return g, nodes, edges
 
 
-def create_graph_components(initial, increments, total):
+def create_graph_components(initial, increments, target):
     nodes = {initial}
     edges = set()
 
@@ -24,7 +26,7 @@ def create_graph_components(initial, increments, total):
 
         for node in tuple(nodes):  # Shallow copy of set
             for increment in increments:
-                new_nodes, new_edges = progress_step(node, total, increment)
+                new_nodes, new_edges = progress_step(node, target, increment)
 
                 edge_count = len(edges)
 
@@ -35,6 +37,9 @@ def create_graph_components(initial, increments, total):
 
         if not changed:
             break
+
+    while trim_graph(nodes, edges, target):  # Trim the graph iteratively until it is finished
+        pass
 
     return nodes, edges
 
@@ -55,3 +60,24 @@ def progress_step(start, stop, step):
         node += step
 
     return nodes, edges
+
+
+def trim_graph(nodes, edges, target):
+    def is_not_stub(n):  # Find out whether R(n,_)
+        return any(filter(lambda e: e[0] == n, edges))
+
+    stubs = []
+
+    for node in filter(lambda n: not (n == target or is_not_stub(n)), nodes):
+        stubs.append(node)
+
+    # Find all edges linked to the stubs, e[0] can be ignored because ~R(n,_) [since that was the stub criteria]
+    stub_edges = reduce(list.__add__, map(lambda n: list(filter(lambda e: e[1] == n, edges)), stubs), [])
+
+    for stub in stubs:
+        nodes.remove(stub)
+
+    for edge in stub_edges:
+        edges.remove(edge)
+
+    return len(stubs) > 0
